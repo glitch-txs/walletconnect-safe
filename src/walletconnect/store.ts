fdea5963c5595a1e972c86ff69d1a8a3c7fe8442
Glitch <glitch.txs@gmail.com>
2023-07-16 07:10:08 -0300

    ethereum provider setup

---
+++ src/walletconnect/store.ts
@@ -0,0 +1,38 @@
+import {
+  EthereumProvider,
+  REQUIRED_EVENTS,
+  REQUIRED_METHODS,
+  OPTIONAL_EVENTS,
+  OPTIONAL_METHODS
+} from '@walletconnect/ethereum-provider'
+
+type Callback = ()=>void
+
+let provider: InstanceType<typeof EthereumProvider>;
+let callback: Callback;
+let getCallback = ()=>callback;
+
+async function InitProvider(){
+  if(typeof window === 'undefined') return
+
+  provider = await EthereumProvider.init({
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID as string,
+    optionalChains:[1, 5, 56, 42161],
+    showQrModal: true,
+    optionalMethods:[...REQUIRED_METHODS, ...OPTIONAL_METHODS],
+    optionalEvents:[...REQUIRED_EVENTS, ...OPTIONAL_EVENTS],
+  })
+
+  //useSyncExternalStore callback
+  getCallback()()
+}
+
+InitProvider()
+
+export const store = {
+  getProvider: ()=>provider,
+  subscribe: (cb: Callback)=>callback = cb  
+}
+
+
+
